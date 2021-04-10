11b73fc2386315a24cef1a64492cd222ddb25d19
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-04-11 00:34:06 +0900

    fix: remove invalid comment

--- src/Trimmer.tsx
+++ src/Trimmer.tsx
@@ -137,7 +137,6 @@ export class Trimmer extends React.Component<TrimmerProps, TrimmerState> {
                 <OsuFileNameInput
                     onChange={(e) => this.setState({ outputFilename: e })}
                 />
-                //TODO
                 <PathInput
                     onChange={(e) =>
                         this.setState({
